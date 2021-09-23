4ae5a7006a68781813d1803923971dfd3aec5905
theodot <theodot@outlook.com>
2021-09-23 13:39:09 -0400

    Add project files.

---
+++ BoxField/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BoxField
+{
+    class Box
+    {
+        public int x, y, size, speed;
+        public SolidBrush brushColour;
+
+        public Box(int _x, int _y, int _size, int _speed, SolidBrush _brushColor)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+            speed = _speed;
+            brushColour = _brushColor;
+        }
+    }
+}
---
+++ BoxField/Screens/GameScreen.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxField
+{
+    public partial class GameScreen : UserControl
+    {
+        //player1 button control keys
+        Boolean leftArrowDown, rightArrowDown;
+
+        //create a list to hold a column of boxes   
+        List<Box> boxes = new List<Box>();
+
+        int newBoxCounter = 0;
+
+        Random randGen = new Random();
+
+        public GameScreen()
+        {
+            InitializeComponent();
+            OnStart();
+        }
+
+        /// <summary>
+        /// Set initial game values here
+        /// </summary>
+        public void OnStart()
+        {
+            CreateBox(250);
+            CreateBox(750);
+        }
+
+        public void CreateBox(int x)
+        {
+            SolidBrush boxBrush = new SolidBrush(Color.White);
+            int colourValue = randGen.Next(1, 4);
+
+            if (colourValue == 1)
+            {
+                boxBrush = new SolidBrush(Color.Red);
+            }
+            else if (colourValue == 2)
+            {
+                boxBrush = new SolidBrush(Color.Yellow);
+            }
+            else
+            {
+                boxBrush = new SolidBrush(Color.Orange);
+            }
+
+            //Box(x, y, size, speed, brush)
+            Box b = new Box(x, 0, 50, 10, boxBrush);
+            boxes.Add(b);
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //player 1 button presses
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;           
+            }
+        }
+
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            newBoxCounter++;
+
+            //update location of all boxes (drop down screen)
+            foreach (Box b in boxes)
+            {
+                b.y += b.speed;
+            }
+
+            //remove box if it has gone of screen
+            if (boxes[0].y > 200)
+            {
+                boxes.RemoveAt(0);   
+            }
+
+            //add new box if it is time
+            if (newBoxCounter == 10)
+            {
+                CreateBox(250);
+                CreateBox(750);
+
+                newBoxCounter = 0;
+            }
+
+            Refresh();
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            //draw boxes to screen
+            foreach(Box b in boxes)
+            {
+                e.Graphics.FillRectangle(b.brushColour, b.x, b.y, b.size, b.size);          
+            }
+        }
+    }
+}
